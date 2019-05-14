2ffcbf54cace70f7157b0cbadfda128aed2734d1
Talon Jones <taloncjones@gmail.com>
2019-05-14 17:56:25 +0000

    Remove auth token handling
    Will use Facebook or another provider for sign in requests.

--- catalog_db.py
+++ catalog_db.py
@@ -5,52 +5,18 @@
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
-from passlib.apps import custom_app_context as pwd_context
-import random, string
-from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
 
 Base = declarative_base()
-secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 
-# Create User object with id, username, picture, email, and password_hash
+# Create User object with id, username, picture, and email
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     username = Column(String(32), index=True)
     picture = Column(String)
     email = Column(String)
-    password_hash = Column(String(64))
-
-    # Create hash of password and store
-    def hash_password(self, password):
-        self.password_hash = pwd_context.encrypt(password)
-
-    # Verify hash of supplied password with stored password hash
-    def verify_password(self, password):
-        return pwd_context.verify(password, self.password_hash)
-
-    # Create authorization token using random 32 bit secret key, with 10 minute (600s) lifetime
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(secret_key, expires_in = expiration)
-        return s.dumps({'id': self.id })
-
-    # Make static method so User entry does not have to exist to attempt token verification
-    @staticmethod
-    def verify_auth_token(token):
-        s = Serializer(secret_key)
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            # Valid Token, but expired
-            return None
-        except BadSignature:
-            # Invalid Token
-            return None
-        # Token is valid and not expired, use 'id' extracted from token as user_id
-        user_id = data['id']
-        return user_id
 
 # Create Category entry with id, name, user_id (from User), and table relationship (User)
 class Category(Base):
