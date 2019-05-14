78fc5e7abc7cabc3132e187c60d71f708640cd45
Talon Jones <taloncjones@gmail.com>
2019-05-14 16:49:39 +0000

    Init catalog_db.py from mytemplate/db_user_initializer.py

---
+++ catalog_db.py
@@ -0,0 +1,61 @@
+#!/user/bin/env python
+
+# catalog_db.py for Udacity Course 4 Item Catalog project
+# Created by Talon Jones
+
+from sqlalchemy import Column,Integer,String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+import random, string
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+Base = declarative_base()
+secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+
+# Create User object with id, username, picture, email, and password_hash
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+    password_hash = Column(String(64))
+
+    # Create hash of password and store
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    # Verify hash of supplied password with stored password hash
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    # Create authorization token using random 32 bit secret key, with 10 minute (600s) lifetime
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in = expiration)
+        return s.dumps({'id': self.id })
+
+    # Make static method so User entry does not have to exist to attempt token verification
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        # Token is valid and not expired, use 'id' extracted from token as user_id
+        user_id = data['id']
+        return user_id
+
+
+# Create database with name x. Change x to desired db name
+engine = create_engine('sqlite:///x.db')
+
+
+Base.metadata.create_all(engine)
+
