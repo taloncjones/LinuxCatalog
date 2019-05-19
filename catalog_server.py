ff94fff01ebb9ff900b59a4e5e010cb0c15cdcb4
Talon Jones <taloncjones@gmail.com>
2019-05-19 02:12:51 +0000

    Add disconnect() from OAuth2.0 project

--- catalog_server.py
+++ catalog_server.py
@@ -50,6 +50,28 @@ def showLogin():
     login_session['state'] = state
     return "The current session state is %s" % login_session['state']
 
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['access_token']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully been logged out.")
+        return redirect(url_for('showRestaurants'))
+    else:
+        flash("You were not logged in")
+        return redirect(url_for('showRestaurants'))
+
+
 ###
 # API functions
 ###
