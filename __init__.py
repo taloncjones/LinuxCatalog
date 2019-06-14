64a22c555a4c807e40d18a95b9618a38148ef760
Talon Jones <taloncjones@gmail.com>
2019-06-14 20:48:30 +0000

    Update fb_client_secrets.json to absolute path

--- __init__.py
+++ __init__.py
@@ -389,10 +389,10 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-    app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
+    app_id = json.loads(open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
-        open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+        open('/var/www/catalog/catalog/fb_client_secrets.json', 'r').read())['web']['app_secret']
     url = 'https://graph.facebook.com/oauth/access_token?grant_type' \
           '=fb_exchange_token&client_id=%s&client_secret=%s' \
           '&fb_exchange_token=%s' % (app_id, app_secret, access_token)
